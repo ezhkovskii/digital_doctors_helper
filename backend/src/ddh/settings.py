c833e24fc2444ac7ae519c6cf22411f75f213624
alexander <ezhkovskii@gmail.com>
2023-05-21 18:09:54 +0300

    delete permissions IsAuthenticated

--- backend/src/ddh/settings.py
+++ backend/src/ddh/settings.py
@@ -166,9 +166,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
-    ],
+    # "DEFAULT_PERMISSION_CLASSES": [
+    #     "rest_framework.permissions.IsAuthenticated",
+    # ],
 }
 
 # CORS_ALLOWED_ORIGINS = os.environ.get("DJANGO_CORS_ALLOWED_ORIGINS", default="").split(" ")
